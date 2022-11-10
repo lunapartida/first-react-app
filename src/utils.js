6c013a80e42e288091f606c6f8bfa04c73823a3f
Luna Partida <lunapartida93@icloud.com>
2022-11-10 14:37:51 -0800

    utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 export const getMovieByName = async (movieName) => {
-  const result = await fetch();
+  const result = await fetch(
+    `http://www.omdbapi.com/?t=${movieName}&apikey=10afb4be`
+  );
 
   const movie = await result.json();
   console.log(movie);

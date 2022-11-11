7f4771a95326122eaf167b010d2fbadf9d0e04cb
Luna Partida <lunapartida93@icloud.com>
2022-11-10 18:27:33 -0800

    day 2 work

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,34 @@
-export const getMovieByName = async (movieName) => {
-  const result = await fetch(
-    `http://www.omdbapi.com/?t=${movieName}&apikey=10afb4be`
+import {} from './services/movies';
+
+export async function getMoviesByName() {
+  const res = await fetch(
+    'http://www.omdbapi.com/?apikey=9f69d263&t=spiderman'
   );
+  const movies = await res.json();
+  console.log(movies);
+}
+// getMoviesByName();
 
-  const movie = await result.json();
-  console.log(movie);
-};
+export async function getMoviesById() {
+  const res = await fetch(
+    'http://www.omdbapi.com/?apikey=9f69d263&i=tt0100669'
+  );
+  const movies = await res.json();
+  console.log(movies);
+}
 
-getMovieByName("napoleon-dynamite");
+// getMoviesById();
+
+
+
+
+// export const getMovieByName = async (movieName) => {
+//   const result = await fetch(
+//     `http://www.omdbapi.com/?t=${movieName}&apikey=10afb4be`
+//   );
+
+//   const movie = await result.json();
+//   console.log(movie);
+// };
+
+// getMovieByName("napoleon-dynamite");
